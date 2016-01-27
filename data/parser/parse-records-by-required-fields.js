ffb03f3f504fd019c32b7ff09b4a5d704f9dd8de
Danny Delott <dannydelott@gmail.com>
2016-01-26 23:33:43 -0800

    Move execution code into IFFE

--- data/parser/parse-records-by-required-fields.js
+++ data/parser/parse-records-by-required-fields.js
@@ -5,33 +5,35 @@
  * USAGE:
  * node parse-records-by-required-fields.js -f <field1> <field2> -o <outputfilename>
  */
-var now   = require('performance-now'),
-    utils = require('./utils');
+(function parse_records_by_required_fields(){
+  var now   = require('performance-now'),
+      utils = require('./FileUtils');
 
-var START_TIMER = now();
-var REQUIRED_FIELDS = parseRequiredFieldsFromCommandLine(process.argv);
-var OUTPUT_FILE = process.argv[process.argv.length-1];
+  var START_TIMER = now();
+  var REQUIRED_FIELDS = parseRequiredFieldsFromCommandLine(process.argv);
+  var OUTPUT_FILE = process.argv[process.argv.length-1];
 
-var files = utils.generateEnumeratedFileNames('../raw/json/food-facts', 'json', 7);
+  var files = utils.generateEnumeratedFileNames('../raw/json/food-facts', 'json', 7);
 
-console.log('Loading data from', files.length, 'file(s)...');
-var data = utils.loadFiles(files);
-var dataMerged = [].concat.apply([], data);
+  console.log('Loading data from', files.length, 'file(s)...');
+  var data = utils.loadFiles(files);
+  var dataMerged = [].concat.apply([], data);
 
-console.log('Looking up records...');
-var cleanData = parseRecordsByRequiredFields(dataMerged, REQUIRED_FIELDS);
-var cleanDataMerged = [].concat.apply([], cleanData);
+  console.log('Looking up records...');
+  var cleanData = parseRecordsByRequiredFields(dataMerged, REQUIRED_FIELDS);
+  var cleanDataMerged = [].concat.apply([], cleanData);
 
-console.log('Exporting results...');
-utils.exportRecords(cleanDataMerged, OUTPUT_FILE);
+  console.log('Exporting results...');
+  utils.exportRecords(cleanDataMerged, OUTPUT_FILE);
 
-var END_TIMER = now();
-var TIME = END_TIMER - START_TIMER;
+  var END_TIMER = now();
+  var TIME = END_TIMER - START_TIMER;
 
-console.log('Total # of rows:\t' + dataMerged.length);
-console.log('Total # of results:\t' + cleanDataMerged.length);
-console.log('Required fields:\t' + REQUIRED_FIELDS.join('\t'));
-console.log('Execution time:\t\t' + utils.millisecondsToTime(TIME));
+  console.log('Total # of rows:\t' + dataMerged.length);
+  console.log('Total # of results:\t' + cleanDataMerged.length);
+  console.log('Required fields:\t' + REQUIRED_FIELDS.join('\t'));
+  console.log('Execution time:\t\t' + utils.millisecondsToTime(TIME));
+})();
 
 /**
  * Returns only records that contain a value for all the specified fields.
