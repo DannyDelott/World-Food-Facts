a95f7ef03a9d514a1825fd9d719f880ec6476378
Danny Delott <dannydelott@gmail.com>
2016-02-04 08:44:16 -0800

    Remove multiline char from last line

--- data/queries.sh
+++ data/queries.sh
@@ -10,5 +10,5 @@ node $PARSER -f countries proteins_100g -o results/proteins-100g-by-country.json
   -f countries product_name sodium_100g -o results/sodium-100g-by-country-with-product-name.json \
   -f countries product_name sugars_100g -o results/sugars-100g-by-country-with-product-name.json \
   -f countries product_name fat_100g -o results/fat-100g-by-country-with-product-name.json \
-  -f countries product_name saturated_fat_100g -o results/saturated-fat-100g-by-country-with-product-name.json \
+  -f countries product_name saturated_fat_100g -o results/saturated-fat-100g-by-country-with-product-name.json
 
