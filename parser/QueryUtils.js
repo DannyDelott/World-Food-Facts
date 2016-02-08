0e052b4d2bbeddd1ef598a4f7fea4ed00f553a19
Danny Delott <dannydelott@gmail.com>
2016-02-07 20:44:34 -0800

    Refactor to function declarations, fix docs

--- parser/QueryUtils.js
+++ parser/QueryUtils.js
@@ -8,7 +8,7 @@
  * @param {Array<Object>} data - the records to process
  * @return {Object} result - the set of data containg the results
  */
-function sumOverMeasure(dimension, measure, data) {
+var sumOverMeasure = function (dimension, measure, data) {
   var result = {};
   data.forEach(function(set) {
     if (!result[set.dimension]) {
@@ -18,33 +18,33 @@ function sumOverMeasure(dimension, measure, data) {
     }
   });
   return result;
-}
+};
 
 /**
- * Return the result of run each query.
+ * Return the result of running a single query.
  * @param {Object} query - the query to perform
  * @param {Array<Object>} data - the records to process
  * @return {Array<Object>} results - the matching rows for the query
  */
-function runQuery (query, data) {
+var runQuery = function (query, data) {
   return data.filter(function(row){
-    return rowContainsRequiredFields(row, query.fields);
+    return _rowContainsRequiredFields(row, query.fields);
   });
-}
+};
 
 /**
- * Return the result of run each query.
+ * Return the results of running multiple queries.
  * @param {Array<Object>} queries - the queries to perform
  * @param {Array<Object>} data - the records to process
  * @return {Array<Array<Object>>} results - the matching rows per query
  */
-function runQueries (queries, data) {
+var runQueries = function (queries, data) {
   return queries.reduce(function(result, query) {
    var matchingRows = runQuery(query, data);
    result.push(matchingRows);
    return result;
   }, []);
-}
+};
 
 /**
  * Return true or false if the row contains the required fields.
@@ -52,10 +52,10 @@ function runQueries (queries, data) {
  * @param {Array<String>} requiredFields - the fields that must be present in the row
  * @return {Boolean} containsRequiredFields
  */
-function rowContainsRequiredFields(row, requiredFields){
+var _rowContainsRequiredFields = function (row, requiredFields) {
   return requiredFields.reduce(function(hasFields, field){
     return hasFields ? row.hasOwnProperty(field) && row[field] : false;
   }, true);
-}
+};
 
 module.exports = { sumOverMeasure, runQuery, runQueries };
