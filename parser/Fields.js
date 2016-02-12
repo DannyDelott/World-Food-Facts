a96f1776a96911cdcc4ea009f6c45d17cd9ced13
Danny Delott <dannydelott@gmail.com>
2016-02-12 09:16:28 -0800

    Create Fields class

---
+++ parser/Fields.js
@@ -0,0 +1,14 @@
+/**
+ * Holds the specs about a query's field requirements 
+ *
+ * @param {Array<String>} fields - the fields or column names
+ * @param {String} mode="ALL" - matching rows must contain values for all fields 
+ */
+var Fields = function(fields, mode){
+  this.fields = fields ? fields : this.fields;
+  this.mode = mode ? mode : this.mode;
+};
+Query.prototype.fields = [];
+Query.prototype.mode = 'ALL';
+
+module.exports = Fields;
