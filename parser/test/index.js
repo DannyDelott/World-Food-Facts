33d18ce035340c3fd6a956ba8a6b9e5975323b38
Danny Delott <dannydelott@gmail.com>
2016-02-13 10:54:07 -0800

    Run tests in order

---
+++ parser/test/index.js
@@ -0,0 +1,6 @@
+require('./Fields.spec.js');
+require('./Query.spec.js');
+require('./QueryResult.spec.js');
+require('./QueryUtils.spec.js');
+require('./FileUtils.spec.js');
+require('./ParserUtils.spec.js');
