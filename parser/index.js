8420e84278c988d2f1ce1520579f6220080360b6
Danny Delott <dannydelott@gmail.com>
2016-02-24 00:19:21 -0800

    Run query from file then export results

--- parser/index.js
+++ parser/index.js
@@ -1,12 +1,20 @@
 var file = '../data/foodfacts.db';
+var outputFile = '../data/result.json';
+var query = require('../data/query');
+
+var fs = require('fs');
 var Utils = require('./utils');
 var db = Utils.loadDatabase(file);
 
+console.log(query);
+
 Utils
-  .runQuery(db, 'SELECT * FROM FoodFacts WHERE LENGTH(countries) > 0 AND trans_fat_100g > 0.0;')
-  .then(function(rows) {
-    console.log('# of matching rows:', rows.length);
-  })
+  .runQuery(db, query)
+  .then(_exportResults)
   .catch(function(e){
     console.log(e);
   });
+
+function _exportResults(results) {
+  fs.writeFileSync(outputFile, JSON.stringify(results));
+}
