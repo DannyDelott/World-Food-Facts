48d0678d1d160edf81c4c92b8cdf8dca603e77c5
Danny Delott <dannydelott@gmail.com>
2016-03-05 12:07:25 -0800

    Add test for #loadDatabase

--- parser/test/utils.spec.js
+++ parser/test/utils.spec.js
@@ -8,12 +8,16 @@ describe('Utils', function() {
   var db;
 
   describe('#loadDatabase', function() {
+    it('should throw an error if the input is not a string', function(){
+      var loadDatabase = Utils.loadDatabase.bind(null, 23);
+      expect(loadDatabase).toThrow('23 not found.');
+    });
     it('should throw an error if the file does not exist', function(){
-      var loadDatabase = Utils.loadDatabase.bind(null, 'foo');
-      expect(loadDatabase).toThrow('foo not found.');
+      var loadDatabase = Utils.loadDatabase.bind(null, 'foo.db');
+      expect(loadDatabase).toThrow('foo.db not found.');
     });
     it('should load and return a sqlite3 database object', function() {
-      db = Utils.loadDatabase('./data/mock/mock.db');
+      db = Utils.loadDatabase(__dirname + '/mock/mock.db');
       expect(db).toBeA(sqlite3.Database);
     });
   });
