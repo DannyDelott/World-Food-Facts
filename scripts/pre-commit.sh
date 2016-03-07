83a12fd541308ea44396cb9374b209ea9d379e7e
Danny Delott <dannydelott@gmail.com>
2016-03-06 20:24:32 -0800

    Refactor pre-commit hook
    - Bail after first file fails the linter

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -4,6 +4,9 @@ for STAGED in $(git diff --cached --name-only)
 do
     if [[ "$STAGED" == *\.js$* ]]; then
         ./node_modules/jscs/bin/jscs $STAGED
+        if (( "$?" != 0 )); then
+            exit 1
+        fi
     fi
 done
 
