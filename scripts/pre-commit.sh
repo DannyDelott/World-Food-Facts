177e830d6ce5f65a2374075062ce261963ce86f0
Danny Delott <dannydelott@gmail.com>
2016-03-31 00:12:23 -0700

    Fix bug that was attempting to lint removed files

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,7 +2,7 @@
 
 for STAGED in $(git diff --cached --name-only)
 do
-    if [[ "$STAGED" == *\.js$* ]]; then
+    if [[ "$STAGED" == *\.js$* ]] && [ -f $STAGED ]; then
         ./node_modules/jscs/bin/jscs $STAGED
         if (( "$?" != 0 )); then
             exit 1
